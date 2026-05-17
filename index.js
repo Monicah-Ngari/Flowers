0ba1bc0ef17713ecc5e6edb09f150e7eb5469fdc
Monicah.Ngari <wakarimangari@gmail.com>
2026-05-17 14:39:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeName(){
+    const names = document.querySelectorAll('.name');
+    names[0].textContent = "Monic";
+    names[1].textContent = "is a";
+    names[2].textContent = "developer";
+}
