24455503a8abe7aa66a08b44159e47d1cf2d6673
Monicah-Ngari <wakarimangari@gmail.com>
2026-05-18 12:44:31 +0300

    added some js functionality

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function changeName(){
-    const names = document.querySelectorAll('.name');
-    names[0].textContent = "Monic";
-    names[1].textContent = "is a";
-    names[2].textContent = "developer";
+function showNatureLove(){
+    const emojis = ["🌿", "🍃", "🌻", "🌸", "😊", "💛"];
+        for (let i = 0; i < 20; i++) {
+        let emoji = document.createElement("span");
+        emoji.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+
+        emoji.style.position = "fixed";
+        emoji.style.left = Math.random() * window.innerWidth + "px";
+        emoji.style.top = Math.random() * window.innerHeight + "px";
+
+        emoji.style.fontSize = "30px";
+        emoji.style.animation = "floatUp 2s ease-out forwards";
+
+        document.body.appendChild(emoji);
+
+        setTimeout(() => {
+            emoji.remove();
+        }, 2000);
+    }
+}
+function viewFlowers() {
+    document.querySelector("h2:nth-of-type(2)").scrollIntoView({ behavior: 'smooth' });
+}
+function viewSunsets() {
+    document.querySelector(".viewsunsets").scrollIntoView({ behavior: 'smooth' });
+}
+function learnMore() {
+    document.querySelector("h3").scrollIntoView({ behavior: 'smooth' });
 }
